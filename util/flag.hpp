907e766f48979deeaf1874a26fa4c966d05e1979
Christian Harper-Cyr <charpercyr@gmail.com>
2017-11-02 19:57:32 -0400

    Little flag.hpp refactor

--- util/flag.hpp
+++ util/flag.hpp
@@ -9,44 +9,41 @@ namespace dyntrace
     struct is_flag_enum : std::false_type {};
     template<typename E>
     constexpr bool is_flag_enum_v = is_flag_enum<E>::value;
-
-#define __ENABLE_IF_FLAG_ENUM(E, R) \
-    std::enable_if_t<is_flag_enum_v<E>, R>
+    template<typename E, typename T>
+    using enable_if_flag_enum_t = std::enable_if_t<is_flag_enum_v<E>, T>;
 
     template<typename E>
-    constexpr __ENABLE_IF_FLAG_ENUM(E, E) operator|(E e1, E e2) noexcept
+    constexpr enable_if_flag_enum_t<E, E> operator|(E e1, E e2) noexcept
     {
         using I = typename std::underlying_type<E>::type;
         return static_cast<E>(static_cast<I>(e1) | static_cast<I>(e2));
     }
 
     template<typename E>
-    constexpr __ENABLE_IF_FLAG_ENUM(E, E) operator&(E e1, E e2) noexcept
+    constexpr enable_if_flag_enum_t<E, E> operator&(E e1, E e2) noexcept
     {
         using I = typename std::underlying_type<E>::type;
         return static_cast<E>(static_cast<I>(e1) & static_cast<I>(e2));
     }
 
     template<typename E>
-    constexpr __ENABLE_IF_FLAG_ENUM(E, E&) operator|=(E& e1, E e2) noexcept
+    constexpr enable_if_flag_enum_t<E, E&> operator|=(E& e1, E e2) noexcept
     {
         return (e1 = e1 | e2);
     }
 
     template<typename E>
-    constexpr __ENABLE_IF_FLAG_ENUM(E, E&) operator&=(E& e1, E e2) noexcept
+    constexpr enable_if_flag_enum_t<E, E&> operator&=(E& e1, E e2) noexcept
     {
         return (e1 = e1 & e2);
     }
 
     template<typename E>
-    constexpr __ENABLE_IF_FLAG_ENUM(E, bool) flag(E e, E f) noexcept
+    constexpr enable_if_flag_enum_t<E, bool> flag(E e, E f) noexcept
     {
         using I = typename std::underlying_type<E>::type;
         return static_cast<I>(e & f) != 0;
     }
-
-#undef __ENABLE_IF_FLAG_ENUM
 }
 
 #endif
