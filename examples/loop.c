605200f3aab9e5ca62bfb86823ac0c9d8b985b25
Christian Harper-Cyr <charpercyr@gmail.com>
2017-11-08 15:52:45 -0500

    Cleaned loop.c

--- examples/loop.c
+++ examples/loop.c
@@ -1,32 +1,5 @@
 #include <stdio.h>
-
 #include <unistd.h>
-#include <stdlib.h>
-
-void* test_malloc(size_t size)
-{
-    printf("malloc %lu\n", size);
-    void* res = malloc(size);
-    printf("returned %p\n", res);
-    return res;
-}
-
-void* test_free(void* ptr)
-{
-    printf("free %p\n", ptr);
-    free(ptr);
-    printf("returned\n");
-}
-
-void* __libc_dlopen_mode(const char*, int);
-
-void* test_dlopen(const char* path, int mode)
-{
-    printf("dlopen %s(%p) %d\n", path, path, mode);
-    void* res = __libc_dlopen_mode(path, mode);
-    printf("returned %p\n", res);
-    return res;
-}
 
 int main()
 {
